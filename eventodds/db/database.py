dc021a6c27e5cc7b646aaf74c536b54363890bb5
Prem-sharma34 <ps4183085@gmail.com>
2026-04-24 02:10:25 +0530

    connect alembic migration with database successfully

--- eventodds/db/database.py
+++ eventodds/db/database.py
@@ -1,8 +1,14 @@
 import os
-from sqlalchemy.orm import DeclarativeBase , sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase , sessionmaker 
 from eventodds.config import settings
 
+engine = create_engine(settings.DATABASE_URL)
 
+sessionLocal = sessionmaker(
+    bind = engine,
+    autoflush=False
+)
 class Base(DeclarativeBase):
     pass
 
