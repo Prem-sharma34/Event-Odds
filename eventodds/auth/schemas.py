a150de164041b4f69f5aca2765d2b25967ab72b4
Prem-sharma34 <ps4183085@gmail.com>
2026-08-04 15:02:16 +0530

    auth setup fix

--- eventodds/auth/schemas.py
+++ eventodds/auth/schemas.py
@@ -1,1 +1,12 @@
 # this files is for authentication related schemas like register/login/logout
+
+from pydantic import BaseModel, EmailStr
+
+
+class User(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+    full_name: str | None = None
+
+
